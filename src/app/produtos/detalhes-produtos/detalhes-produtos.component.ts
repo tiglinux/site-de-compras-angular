6a0f195f06f547400be9b4bd49845dd0eeebbf72
Santos <tiago.b.santos@capgemini.com>
2022-10-14 17:22:01 -0300

    Alteração no Assets por tiago

---
+++ src/app/produtos/detalhes-produtos/detalhes-produtos.component.ts
@@ -0,0 +1,42 @@
+import { CarrinhoService } from 'src/app/carrinho.service';
+import { NotificacaoService } from 'src/app/notificacao.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProdutosService } from 'src/app/produtos.service';
+import { Component, OnInit } from '@angular/core';
+import { IProduto, IProdutoCarrinho } from 'src/app/produtos';
+
+@Component({
+  selector: 'app-detalhes-produtos',
+  templateUrl: './detalhes-produtos.component.html',
+  styleUrls: ['./detalhes-produtos.component.css']
+})
+export class DetalhesProdutosComponent implements OnInit {
+  produto: IProduto | undefined;
+  quantidade = 1;
+
+  constructor(
+    private produtosService: ProdutosService,
+     private route: ActivatedRoute,
+     private notificacaoService: NotificacaoService,
+     private carrinhoService: CarrinhoService
+    ) { }
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho");
+    const produto:IProdutoCarrinho = {
+      ...this.produto!,
+      quantidade: this.quantidade
+
+    }
+    this.carrinhoService.adicionarAoCarrinho(produto);
+
+  }
+
+}
